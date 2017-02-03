75335cfd83feebd4f3c7d54f20f7e934d5752ddb
Jean Yang <bdlas321@gmail.com>
2017-02-03 22:10:43 +0800

    unit test spec for Slogan.vue

--- test/unit/specs/Slogan.spec.js
+++ test/unit/specs/Slogan.spec.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
-import Hello from 'src/components/Hello';
+import Slogan from 'src/components/Slogan';
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
       el: document.createElement('div'),
-      render: (h) => h(Hello),
+      render: h => h(Slogan),
     });
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App');
+    expect(vm.$el.querySelector('.slogan h1').textContent)
+      .to.equal('The Simplest but Funniest Game.');
   });
 });
